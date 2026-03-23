13b8f82ec98ff976d0facad680e78dd9fb718fb8
Licza <llobo2@emory.edu>
2026-03-23 17:42:50 -0400

    Hi Liam, I completed my analysis, all under my subproject folder!

---
+++ subproject_licza/code/01_log_reg.R
@@ -0,0 +1,33 @@
+library(readr)
+
+here::i_am("subproject_licza/code/01_log_reg.R")
+
+df <-  read_csv(here::here("data/covid_sub.csv"))
+
+#Does pneumonia increase an individual's risk of whether the patient was connected to the ventilator 
+# (controlling for age, sex, and presence of asthma) ?
+
+log_df <- df |>
+  mutate(
+    INTUBED = case_when(INTUBED == "Yes" ~ 1, INTUBED == "No"  ~ 0, TRUE ~ NA_real_),
+    SEX = case_when(SEX== 'male' ~ 1, SEX == 'female' ~ 0, TRUE ~ NA_real_),
+    ASTHMA = case_when(ASTHMA== 'Yes' ~ 1, ASTHMA == 'No' ~ 0, TRUE ~ NA_real_),
+    PNEUMONIA = case_when(PNEUMONIA== 'Yes' ~ 1, PNEUMONIA == 'No' ~ 0, TRUE ~ NA_real_),
+    )
+
+binary_mod <- glm(
+  INTUBED ~ PNEUMONIA + AGE + SEX + ASTHMA,
+  data = log_df,
+  family = binomial()
+)
+
+reg_table <- tbl_regression(binary_mod, exponentiate = TRUE) |>
+  add_global_p()
+
+saveRDS(
+  reg_table,
+  file = here::here("subproject_licza/output/regression_table.rds")
+)
+
+
+
---
+++ subproject_licza/code/02_render_report.R
@@ -0,0 +1,7 @@
+here::i_am(
+  "subproject_licza/code/02_render_report.R"
+)
+
+rmarkdown::render(
+  here::here("subproject_licza/report.Rmd")
+)
